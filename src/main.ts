ef3ec26359a0990e6294110bd45b900ce8a1fb29
Ragil Bayu Respati <bayumoonfang@gmail.com>
2024-06-07 16:48:57 +0700

    first

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {SupabaseClient, createClient} from '@supabase/supabase-js'
+import cors from 'cors';
+import { NotFoundException } from '@nestjs/common';
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -14,19 +16,37 @@ app.use(bodyParser.json());
 const supabase = createClient('https://dnktnvncjdkdglxqfbun.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRua3Rudm5jamRrZGdseHFmYnVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0MDgwMTYsImV4cCI6MjAzMjk4NDAxNn0.7oZl2dzezuWnHB1NaKcXkRDkGP3LL7Rq1EOvnXMQjFs')
 
 app.get('/', (req, res) => {
-    res.send("Hello I am working my friend Supabase <3");
+    res.send("Hello I am working my friend Supabase <3 ssss");
 });
 
-app.get('/users', async (req, res) => {
-    const {data, error} = await supabase
-        .from('users')
-        .select()
-    res.send(data);
+// app.get('/users', async (req, res) => {
+//     const {data, error} = await supabase
+//         .from('users')
+//         .select()
+//     res.send(data);
+// });
+
+app.post('/users', async (req, res) => {
+    const email = req.body.email; 
+    const password = req.body.password; 
+    const { data, error } = await supabase
+    .from('users')
+    .select('user_id')
+    .eq("user_name", email)
+    .eq("user_passwd", password);
+
+    if(data.length == 0) {
+        res.send('0');
+    } else {
+         //res.send(data);
+          res.send('1');
+    }
+    
 });
 
 
-app.listen(3000, () => {
-    console.log(`> Ready on http://localhost:3000`);
+app.listen(8080, () => {
+    console.log(`> Ready on http://localhost:8080`);
 });
 
 
